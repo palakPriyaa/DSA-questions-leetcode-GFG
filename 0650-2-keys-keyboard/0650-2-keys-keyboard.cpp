f9e778ddd7221f84d125ad936b0fffa685e5f678
Palak Priyadarshni <120651961+palakPriyaa@users.noreply.github.com>
2024-08-19 23:08:34 +0530

    Time: 0 ms (100%), Space: 7.6 MB (55.97%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minSteps(int n) 
+    {
+        int ans = 0;
+        for(int i=2;i*i<=n;i++)
+        {
+            while(n % i == 0)
+            {
+                ans += i;
+                n /= i;
+            }
+        }
+        if(n > 1) ans += n;
+        return ans;
+    }
+};
