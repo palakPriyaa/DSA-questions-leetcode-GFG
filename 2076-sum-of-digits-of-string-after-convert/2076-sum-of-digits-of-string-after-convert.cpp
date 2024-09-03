684404ee9e45c0dc4f5f09b0f71a56c047c55f8e
Palak Priyadarshni <120651961+palakPriyaa@users.noreply.github.com>
2024-09-03 17:29:14 +0530

    Time: 4 ms (39.15%), Space: 7.8 MB (90.64%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int getLucky(string& s, int k) {
+        int num=0;
+       
+        for (char c: s){
+            int x=c-'a'+1;
+            auto [q, r]=div(x, 10);
+            num+=q+r;
+        }
+  
+        if (k==1) return num;
+        k--;
+        for(int x=num; x>=10 && k>0; k--){
+            for(num=0; x>0; ){
+                auto [q, r]=div(x, 10);
+                num+=r;
+                x=q;
+            }
+   
+            x=num;
+        }
+        return num;
+    }
+};
