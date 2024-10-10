facf89e3d41717b95fe591eae73e72caea9bca56
Palak Priyadarshni <120651961+palakPriyaa@users.noreply.github.com>
2024-10-10 15:00:03 +0530

    Time: 4 ms (84.75%), Space: 16.9 MB (65.38%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-void dfs(vector<vector<int>>& image, int i, int j,int val, int newColor)
+   void dfs(vector<vector<int>>& image, int i, int j,int val, int newColor)
 {
     if(i<0 || i>=image.size() || j<0 || j>= image[0].size() ||
        image[i][j] == newColor || image[i][j] != val)
@@ -16,8 +16,9 @@ void dfs(vector<vector<int>>& image, int i, int j,int val, int newColor)
     
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor)
     {
-        int val = image[sr][sc];
-        dfs(image,sr,sc,val,newColor);
-        return image;
+      int val = image[sr][sc];
+       dfs(image ,sr , sc , val, newColor);
+       return image;
+
     }
 };
