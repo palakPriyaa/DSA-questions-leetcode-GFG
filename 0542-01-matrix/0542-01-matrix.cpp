03f2d2e51055d8538ec7b9017ccf38bc02bf25f1
Palak Priyadarshni <120651961+palakPriyaa@users.noreply.github.com>
2024-10-11 15:54:29 +0530

    Time: 53 ms (62.1%), Space: 37.6 MB (55.27%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,30 +1,96 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,INT_MAX-2));
-        
-        for (int i=0;i<n;i++){
-            for (int j=0;j<m;j++){
-                if (mat[i][j]==0) dp[i][j]=0;
-                else {
-                    if (i>0) dp[i][j]= min (dp[i][j], dp[i-1][j]+1); 
-                    if (j>0) dp[i][j]=min (dp[i][j],dp[i][j-1]+1); 
+        vector<vector<int>>ans(mat.size(),vector<int>(mat[0].size()));
+        vector<vector<bool>>visited(mat.size(),vector<bool>(mat[0].size(),false));
+        queue<pair<pair<int,int>,int>>q; //{{i,j},distance}
+        for(int i=0; i<mat.size();i++){
+            for(int j=0;j<mat[i].size();j++){
+                if(mat[i][j]==0){
+                    q.push({{i,j},0});
+                    visited[i][j]=true;
                 }
             }
         }
-
-        
-             for (int i=n-1;i>=0;i--){
-            for (int j=m-1;j>=0;j--){
-                if (mat[i][j]==0) dp[i][j]=0;
-                else {
-                    if (i<n-1) dp[i][j]= min (dp[i][j], dp[i+1][j]+1); 
-                    if (j<m-1) dp[i][j]=min (dp[i][j],dp[i][j+1]+1); 
-                }
+        while(!q.empty()){
+            int a = q.front().first.first;
+            int b = q.front().first.second;
+            int dis = q.front().second;
+            q.pop();
+            ans[a][b]=dis;
+            if(a!=0 && !visited[a-1][b]){
+                visited[a-1][b]=true;
+                q.push({{a-1,b},dis+1});
+            }
+            if(a!=mat.size()-1 && !visited[a+1][b]){
+                visited[a+1][b]=true;
+                q.push({{a+1,b},dis+1});
+            }
+            if(b!=0 && !visited[a][b-1]){
+                visited[a][b-1]=true;
+                q.push({{a,b-1},dis+1});
+            }
+            if(b!=mat[0].size()-1 && !visited[a][b+1]){
+                visited[a][b+1]=true;
+                q.push({{a,b+1},dis+1});
             }
         }
-        return dp;
+        return ans;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+//        int n = mat.size();
+//        int m = mat[0].size();
+       
+//        queue<pair<pair<int , int> , int>> q;
+//        vector<vector<int>> vis(n , vector<int> (m , 0));
+//         vector<vector<int>> dist(n , vector<int> (m , 0));
+
+
+//         for(int i =0;i<n;i++){
+//             for(int j =0;j<m;j++){
+//                 if(mat[i][j] == 0){
+//                     q.push({{i, j} , 0});
+//                     vis[i][j] = 1;
+//                 }
+//             }
+//         }
+       
+//         int derow[] = {-1, 0 , 1 , 0};
+//         int decol[] = {-1 , 0 , +1 , 0};
+
+
+//         while(!q.empty()){
+//             int row = q.front().first.first;
+//             int col = q.front().first.second;
+//             int steps = q.front().second;
+//             q.pop();
+
+//             dist[row][col] = steps;
+
+//             for(int i =0;i<4;i++){
+//                int nrow = row + derow[i];
+//                int ncol = col + decol[i];
+
+//                if(nrow >= 0 && nrow < n && ncol >=0  && ncol < m && vis[nrow][ncol] == 0){
+//                 vis[nrow][ncol] = 1;
+//                 q.push({{nrow , ncol} , steps+1});
+
+//                }
+//             }
+//         }
+
+
+//         return dist;
+
+
+
+
+//     }
+// };
