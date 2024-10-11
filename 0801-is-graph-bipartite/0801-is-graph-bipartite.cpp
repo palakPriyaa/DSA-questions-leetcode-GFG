bb96f793a4beaff843b212ab895b55408aae8be9
Palak Priyadarshni <120651961+palakPriyaa@users.noreply.github.com>
2024-10-12 01:19:35 +0530

    Time: 8 ms (98.71%), Space: 16.7 MB (35.42%) - LeetHub

---
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int V = graph.size();
+        vector<int> vis(V, -1); 
+
+        
+        for (int start = 0; start < V; start++) {
+            if (vis[start] == -1) {  
+                queue<int> q;
+                q.push(start);
+                vis[start] = 0;  // Start with color 0
+
+                // Perform BFS
+                while (!q.empty()) {
+                    int node = q.front();
+                    q.pop();
+
+                    // Traverse all adjacent nodes
+                    for (auto neighbor : graph[node]) {
+                        if (vis[neighbor] == -1) {  // If neighbor is unvisited
+                            vis[neighbor] = !vis[node];  // Assign opposite color
+                            q.push(neighbor);
+                        } 
+                        else if (vis[neighbor] == vis[node]) {  // Same color detected
+                            return false;  // Not bipartite
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;  // If no conflicts, it's bipartite
+    }
+};
+
+
+
+// class Solution {
+// public:
+//     bool isBipartite(vector<vector<int>>& graph) {
+//         int V = graph.size();
+//         queue<int> q;
+//         q.push(0);
+
+//         vector<int> vis(V, -1);
+//         vis[0] = 0;
+
+//         while(!q.empty()){
+//             int node = q.front();
+//             q.pop();
+
+//             for(auto i : graph[node]){
+//                 if(vis[i] == -1){
+//                     vis[i] = !vis[node];
+//                     q.push(i);
+//                 }
+
+//                 else if(vis[i] == vis[node]) {
+//                      return false;
+//                 }
+//             }
+//         }
+
+//         return true;
+//     }
+// };
