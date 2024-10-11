796d72a7b2ee8c3d1ba5d41701da14655edc9d81
Palak Priyadarshni <120651961+palakPriyaa@users.noreply.github.com>
2024-10-12 01:35:28 +0530

    Time: 19 ms (46%), Space: 16.3 MB (76.76%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,38 +1,75 @@
+//dfs
+
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        int V = graph.size();
-        vector<int> vis(V, -1); 
+    bool dfs(int node, vector<int>& vis, vector<vector<int>>& graph, int col) {
+        vis[node] = col; 
 
-        
-        for (int start = 0; start < V; start++) {
-            if (vis[start] == -1) {  
-                queue<int> q;
-                q.push(start);
-                vis[start] = 0;  // Start with color 0
-
-                // Perform BFS
-                while (!q.empty()) {
-                    int node = q.front();
-                    q.pop();
-
-                    // Traverse all adjacent nodes
-                    for (auto neighbor : graph[node]) {
-                        if (vis[neighbor] == -1) {  // If neighbor is unvisited
-                            vis[neighbor] = !vis[node];  // Assign opposite color
-                            q.push(neighbor);
-                        } 
-                        else if (vis[neighbor] == vis[node]) {  // Same color detected
-                            return false;  // Not bipartite
-                        }
-                    }
-                }
+        for (auto i : graph[node]) {
+            if (vis[i] == -1) {  
+                if (!dfs(i, vis, graph, !col)) 
+                    return false;
+            } else if (vis[i] == col) {  
+                return false;  
             }
         }
 
-        return true;  // If no conflicts, it's bipartite
+        return true;
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) {
+        int V = graph.size();
+        vector<int> vis(V, -1);  
+
+        
+        for (int i = 0; i < V; i++) {
+            if (vis[i] == -1) {  
+                if (!dfs(i, vis, graph, 0))  
+                    return false;  
+            }
+        }
+
+        return true;  
     }
 };
+
+//bfs
+
+// class Solution {
+// public:
+//     bool isBipartite(vector<vector<int>>& graph) {
+//         int V = graph.size();
+//         vector<int> vis(V, -1); 
+
+        
+//         for (int start = 0; start < V; start++) {
+//             if (vis[start] == -1) {  
+//                 queue<int> q;
+//                 q.push(start);
+//                 vis[start] = 0;  // Start with color 0
+
+//                 // Perform BFS
+//                 while (!q.empty()) {
+//                     int node = q.front();
+//                     q.pop();
+
+//                     // Traverse all adjacent nodes
+//                     for (auto neighbor : graph[node]) {
+//                         if (vis[neighbor] == -1) {  // If neighbor is unvisited
+//                             vis[neighbor] = !vis[node];  // Assign opposite color
+//                             q.push(neighbor);
+//                         } 
+//                         else if (vis[neighbor] == vis[node]) {  // Same color detected
+//                             return false;  // Not bipartite
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+
+//         return true;  // If no conflicts, it's bipartite
+//     }
+// };
 
 
 
