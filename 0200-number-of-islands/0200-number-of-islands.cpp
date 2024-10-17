9519c5cda09232790f770a16623da4c9021dd05f
Palak Priyadarshni <120651961+palakPriyaa@users.noreply.github.com>
2024-10-17 19:52:07 +0530

    Time: 29 ms (54.3%), Space: 17.1 MB (52.22%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
+    void dfs(vector<vector<char>>& grid, vector<vector<int>>& vis, int i, int j) {
+  
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0' || vis[i][j] == 1)
+            return;
+        
+        
+        vis[i][j] = 1;
+        
+        
+        dfs(grid, vis, i+1, j);  // Down
+        dfs(grid, vis, i-1, j);  // Up
+        dfs(grid, vis, i, j+1);  // Right
+        dfs(grid, vis, i, j-1);  // Left
+    }
 
-void dfs(vector<vector<char>>& grid , int row , int col){
-    int rows = grid.size();
-    int cols = grid[0].size();
-
-     if(row < 0 || col < 0 || row >= rows || col >= cols || grid[row][col] != '1')
-                return;
-
-
-            grid[row][col] = '0';
-
-            dfs(grid ,row - 1, col);
-            dfs(grid,row + 1, col);
-            dfs(grid,row, col - 1);
-            dfs(grid,row, col + 1);
-}
     int numIslands(vector<vector<char>>& grid) {
-        if(grid.empty() || grid[0].empty())
-            return 0;
+        int n = grid.size();
+        int m = grid[0].size();
         
-        int rows = grid.size();
-        int cols = grid[0].size();
-        int islands = 0;
+        vector<vector<int>> vis(n, vector<int>(m, 0));  
+        int ans = 0;
         
-        
-        for(int row = 0; row < rows; row++) {
-            for(int col = 0; col < cols; col++) {
-                if(grid[row][col] == '1') {
-                    dfs(grid ,row, col);
-                    islands++;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                
+                if(grid[i][j] == '1' && !vis[i][j]) {
+                    dfs(grid, vis, i, j);  
+                    ans++;  
                 }
             }
         }
-        
-        return islands;
+
+        return ans;
     }
 };
