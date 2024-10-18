b0e6e7845c5562ed4cb25140d96f63921fb89ae5
Palak Priyadarshni <120651961+palakPriyaa@users.noreply.github.com>
2024-10-18 15:34:37 +0530

    Time: 4 ms (99.79%), Space: 19.6 MB (24.73%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,43 +1,53 @@
 class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& pre) {
-        
         vector<vector<int>> adj(n);
-        vector<int> indegree(n, 0);
 
-        // Build the graph (adjacency list) from prerequisites
-        for (auto& p : pre) {
-            int course = p[0];
-            int prereq = p[1];
-            adj[prereq].push_back(course);  // Edge from prereq to course
-            indegree[course]++;             // Increase indegree of course
+        for(auto i : pre){
+            int a = i[0];
+            int b = i[1];
+            adj[b].push_back(a);
+        }
+
+        vector<int> indegree(n , 0);
+
+        for(int i =0;i<n;i++){
+            for(auto it : adj[i])
+            indegree[it]++;
         }
 
         queue<int> q;
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);  
-            }
+        vector<int> topo;
+
+        for(int i =0;i<n;i++){
+            if(indegree[i] ==0 )
+            q.push(i);
         }
 
-        vector<int> topo;  
 
-        // Process nodes in topological order
-        while (!q.empty()) {
+        while(!q.empty()){
             int node = q.front();
             q.pop();
             topo.push_back(node);
 
-            // Reduce the indegree of neighboring nodes (courses dependent on this course)
-            for (int nextCourse : adj[node]) {
-                indegree[nextCourse]--;
-                if (indegree[nextCourse] == 0) {
-                    q.push(nextCourse);  // If indegree becomes 0, it can now be taken
-                }
+            for(auto i : adj[node]){
+                indegree[i]--;
+            
+
+
+            if(indegree[i] == 0)
+            q.push(i);
             }
         }
 
-        // If topological sort includes all courses, return true
-        return topo.size() == n;
+
+
+        if(topo.size() == n)
+        return true;
+
+        return false;
+
+      
+       
     }
 };
