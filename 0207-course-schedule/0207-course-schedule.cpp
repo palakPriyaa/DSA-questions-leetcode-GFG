bb5d0ab47a7d5c46f8266775030cf4d72bda14b7
Palak Priyadarshni <120651961+palakPriyaa@users.noreply.github.com>
2024-10-20 22:28:27 +0530

    Time: 4 ms (99.46%), Space: 19.7 MB (22.85%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,53 +1,54 @@
 class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& pre) {
-        vector<vector<int>> adj(n);
+      vector<vector<int>> adj(n);
 
-        for(auto i : pre){
-            int a = i[0];
-            int b = i[1];
-            adj[b].push_back(a);
-        }
+       for(auto i : pre){
+        int a = i[0];
+        int b = i[1];
+        
+        adj[b].push_back(a);
+       } 
 
-        vector<int> indegree(n , 0);
-
+       //indegree array
+       vector<int> indegree(n ,  0);
+        
         for(int i =0;i<n;i++){
-            for(auto it : adj[i])
-            indegree[it]++;
+          for(auto j : adj[i]){
+            indegree[j]++;
+        }
         }
 
+        //push the elemts who have indegree 0 in the queue
         queue<int> q;
-        vector<int> topo;
 
         for(int i =0;i<n;i++){
-            if(indegree[i] ==0 )
+            if(indegree[i] == 0)
             q.push(i);
         }
+       
+       //topo sort array
+       vector<int> topo;
 
+       while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        topo.push_back(node);
 
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            topo.push_back(node);
-
-            for(auto i : adj[node]){
-                indegree[i]--;
-            
-
+        for(auto i : adj[node]){
+            indegree[i]--;
 
             if(indegree[i] == 0)
             q.push(i);
-            }
         }
+       }
 
 
+       if(topo.size() == n)
+       return true;
 
-        if(topo.size() == n)
-        return true;
+       return false;
 
-        return false;
 
-      
-       
     }
 };
