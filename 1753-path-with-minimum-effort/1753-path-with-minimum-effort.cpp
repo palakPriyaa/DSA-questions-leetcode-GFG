2b4d787502e54617814e8467f14cddef342620ff
Palak Priyadarshni <120651961+palakPriyaa@users.noreply.github.com>
2024-10-30 18:40:03 +0530

    Time: 58 ms (65.51%), Space: 23.8 MB (78.36%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,59 +1,42 @@
-#include <vector>
-#include <queue>
-#include <cmath>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& h) {
-        int m = h.size(), n = h[0].size();
+        int n = h.size();
+        int m = h[0].size();
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
 
-        
         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> q;
-        
-       
-        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
-
-        
-        dist[0][0] = 0;
         q.push({0, {0, 0}});
+        dist[0][0] = 0;
 
-        // Direction vectors for up, down, left, and right moves.
-        int dr[] = {-1, 1, 0, 0};
-        int dc[] = {0, 0, -1, 1};
+        int drow[] = {-1, 1, 0, 0};
+        int dcol[] = {0, 0, -1, 1};
 
         while (!q.empty()) {
             int dis = q.top().first;
-            int r = q.top().second.first;
-            int c = q.top().second.second;
-            
+            int row = q.top().second.first;
+            int col = q.top().second.second;
             q.pop();
 
-            // If we've reached the bottom-right corner, return the effort.
-            if (r == m - 1 && c == n - 1) {
+            // If we reach the target cell with minimum effort, return it
+            if (row == n - 1 && col == m - 1)
                 return dis;
-            }
 
-            
             for (int i = 0; i < 4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
+                int nrow = drow[i] + row;
+                int ncol = dcol[i] + col;
 
-                
-                if (nr >= 0 && nc >= 0 && nr < m && nc < n) {
-                    //  new effort f
-                    int newdis = max(dis, abs(h[nr][nc] - h[r][c]));
+                if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < m) {
+                    int newdis = max(dis, abs(h[row][col] - h[nrow][ncol]));
 
-                    // If the new effort is less than the previously known effort, update it.
-                    if (newdis < dist[nr][nc]) {
-                        dist[nr][nc] = newdis;
-                        q.push({newdis, {nr, nc}});
+                    if (newdis < dist[nrow][ncol]) {
+                        dist[nrow][ncol] = newdis;
+                        q.push({newdis, {nrow, ncol}});
                     }
                 }
             }
         }
-       
-        return 0;
+
+        return -1;
     }
 };
