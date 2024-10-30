4701479b992214f073b91e82503e521cbe951ada
Palak Priyadarshni <120651961+palakPriyaa@users.noreply.github.com>
2024-10-30 17:11:22 +0530

    Time: 6 ms (45.31%), Space: 17.3 MB (72.64%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,50 +1,49 @@
 class Solution {
 public:
     vector<int> findOrder(int n, vector<vector<int>>& pre) {
-        
+
         vector<vector<int>> adj(n);
-        vector<int> indegree(n, 0); 
+          vector<int> indegree(n , 0);
 
-        // Build the graph and indegree array
-        for (auto& i : pre) {
-            int a = i[0];
+        for(auto &i : pre){
             int b = i[1];
+            int a = i[0];
 
-            adj[b].push_back(a);  
-            indegree[a]++;       
+            adj[b].push_back(a);
+            indegree[a]++;
         }
+        
+       
 
         queue<int> q;
-        vector<int> topo;
 
-        // Add all courses with indegree 0 to the queue
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
-            }
+        for(int i =0;i<n;i++){
+            if(indegree[i] == 0)
+            q.push(i);
         }
 
-        // Process courses in topological order
-        while (!q.empty()) {
+         vector<int> topo;
+
+        while(!q.empty()){
             int node = q.front();
             q.pop();
-
             topo.push_back(node);
 
-            for (int neighbor : adj[node]) {
-                indegree[neighbor]--;
+            for(auto i : adj[node]){
+                indegree[i]--;
 
-                if (indegree[neighbor] == 0) {
-                    q.push(neighbor);
-                }
+
+                if(indegree[i] == 0)
+                q.push(i);
             }
-        }
 
-        // If all courses are processed, return the order; otherwise, return an empty list
-        if (topo.size() == n) {
-            return topo;
-        } else {
-            return {};
         }
+     
+
+     if(topo.size() == n)
+     return topo;
+
+     return {};
+
     }
 };
