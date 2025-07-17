62ff8a273703ac5ff001f73e575272b9a170b696
Palak Priyadarshni <120651961+palakPriyaa@users.noreply.github.com>
2025-07-17 16:20:35 +0530

    Time: 5 ms (23.8%), Space: 8.9 MB (31.01%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     int possibleStringCount(string word) {
+        int n = word.size();
         int ans = 1;
-        int count = 1;
-        char last = word[0];
+        int cnt = 1;
 
-        for (int i = 1; i < word.size(); i++) {
-            if (word[i] != last) {
-                ans += count - 1;
-                count = 1;
-                last = word[i];
-            } else {
-                count++;
+        char ch = word[0];
+
+        for(int i = 1; i<n;i++){
+            if(word[i] == ch){
+                cnt++;
             }
+            else{
+               ans += cnt -1;
+               cnt =1;
+               ch = word[i];
+            }
+
+
         }
 
-        ans += count - 1;
+        ans += cnt -1;
+
         return ans;
     }
 };
